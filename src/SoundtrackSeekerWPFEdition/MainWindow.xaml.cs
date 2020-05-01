1ea30615a5be90a86d76bac173df80a51e0a596f
Jack Gilmartin <s00171013@mail.itsligo.ie>
2020-05-01 18:35:52 +0100

    Further album search term optimizations added.

--- src/SoundtrackSeekerWPFEdition/MainWindow.xaml.cs
+++ src/SoundtrackSeekerWPFEdition/MainWindow.xaml.cs
@@ -92,6 +92,8 @@ namespace SoundtrackSeekerWPFEdition
                     // Prepare album name to make it more suitable to search the API with.
                     foundAlbum = foundAlbum.Replace(":", "");
                     foundAlbum = foundAlbum.Replace(".", "");
+                    foundAlbum = foundAlbum.Replace("(", "");
+                    foundAlbum = foundAlbum.Replace(")", "");
 
                     SearchAlbumImage(foundAlbum); // Look for a corresponding album image online.
                 }), DispatcherPriority.Render);
